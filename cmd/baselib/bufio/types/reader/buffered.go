40a52383a64714dd9f7b65240663d8596594515c
cpxI <use.cool.0980@gmail.com>
2022-08-24 15:21:43 +0800

    add code

--- cmd/baselib/bufio/types/reader/buffered.go
+++ cmd/baselib/bufio/types/reader/buffered.go
@@ -10,6 +10,7 @@ func buffered(b []byte) {
 	fmt.Println(fmt.Sprintf(call, "buffered"))
 	br := bytes.NewReader(b)
 	r := bufio.NewReader(br)
+
 	fmt.Println("buffered:", r.Buffered())
 	rb, _ := r.ReadByte()
 	fmt.Println("read:", rb)
